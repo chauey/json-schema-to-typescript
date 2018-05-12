b53caa71d8a2c54c8e5a1c053df5652804b18bdf
Chau (Joe) Nguyen <chauey@hotmail.com>
2018-05-12 17:32:42 +0700

    use oas3 in example

--- example/index.ts
+++ example/index.ts
@@ -1,8 +1,9 @@
-import { writeFileSync } from 'fs'
-import { compileFromFile } from 'json-schema-to-typescript'
+import { writeFileSync } from 'fs';
+import { compileFromFile } from 'json-schema-to-typescript';
 
 async function generate() {
-  writeFileSync('person.d.ts', await compileFromFile('person.json'))
+  // writeFileSync('person.d.ts', await compileFromFile('person.json'))
+  writeFileSync('oas3-interface.ts', await compileFromFile('openapi-3.0.json'))
 }
 
 generate()
